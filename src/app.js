1583733c33ede1ce40c352f151e9bec00b57f7f5
saurabhkmrss585 <saurabhkmrss585@gmail.com>
2024-11-07 19:37:31 +0530

    managing route handlers

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 const express=require('express');
 const app=express();
 
-
-
-
-
-app.listen("7777",()=>{
+app.listen("7777",()=>{ 
     console.log("congrats your server is running ");
 })
-app.use("/hello",(req,res)=>{
-    res.send("hi i am responding on hello");
-})
-app.use("/test",(req,res)=>{
-    res.send("hi i am responding from test ")
-})
+
+app.use("/user",(req,res,next)=>{
+   // res.send("Hi i am first route handler")
+    next();
+},
+(req,res,next)=>{
+    //res.send("hi i am second route handler ");
+    next();
+
+},
+(req,res)=>{
+    res.send("hi i am third route handler");
+}
+)
