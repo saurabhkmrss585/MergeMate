146395560508d667493a896e080acbbd13ead109
saurabhkmrss585 <saurabhkmrss585@gmail.com>
2024-11-09 23:47:40 +0530

    crud on database

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const app=express();
 const User=require("./models/user");
 
 app.use(express.json());
+
 app.post("/signUp",async(req,res)=>{
     //creating new instance of user model
     const user=new User(req.body); 
@@ -14,10 +15,51 @@ app.post("/signUp",async(req,res)=>{
      res.status(400).send("there waserror in saving databse"+err.message);
     }
 })
- 
+
+//get user by email
+app.get("/user",async(req,res)=>{
+  try{
+    const userEmail=req.body.emailId;
+    const users=await User.find({emailId:userEmail})
+    if(users.length===0){
+       res.status(404).send("user not found")
+    }else{
+        res.send(users)
+    }
+  }catch(err){
+    res.send("there was error in receiving message")
+  }
+});
+
+
+//feed api-get all the data from the database
+app.get("/feed",async(req,res)=>{
+     try{
+        const users=await User.find({})
+        res.send(users);
+     }catch(err){
+        res.send("there was error in receiving message")
+     }
 
 
 
+})
+//delete a user
+app.delete("/user",async(req,res)=>{
+const userId=req.body.Id;
+const user=await User.findByIdAndDelete(userId)
+res.send("user deleted successful");
+})
+//update data of the user
+
+
+app.patch("/user",async(req,res)=>{
+    const userId=req.body._id;
+    const data=req.body
+   await User.findByIdAndUpdate({_id:userId},data);
+   res.send("id updated successfully");
+
+})
 
 
 
